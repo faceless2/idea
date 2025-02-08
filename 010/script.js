61ea4b450f6d830670f7f95ca19917f4a1b791e6
Mike Bremford <mike@bfo.com>
2025-02-08 12:52:11 +0000

    Tweaked 010 to show SlippyTree

--- 010/script.js
+++ 010/script.js
@@ -3,48 +3,85 @@
  */
 communityIdeas[communityIdeas.length] = (() => {
     const baseurl = document.currentScript.src.substring(0, document.currentScript.src.lastIndexOf("/"));
-    let link, button, tab;
+    let link1, link2, script1, button, tab;
     let o = {
         name: "Integrated tree-apps",
         load: () => {
-            link = document.createElement("link");
-            link.rel = "stylesheet";
-            link.href = baseurl + "/style.css";
+            link1 = document.createElement("link");
+            link1.rel = "stylesheet";
+            link1.href = baseurl + "/style.css";
+            link2 = document.createElement("link");
+            link2.rel = "stylesheet";
+            link2.href = "https://apps.wikitree.com/apps/bremford24/test/views/slippyTree/style.css";
+            script1 = document.createElement("script");
+            script1.src = "https://apps.wikitree.com/apps/bremford24/test/views/slippyTree/script.js";
             return document.querySelector("#familyVitals") != null;
         },
         activate: () => {
-            document.head.appendChild(link);
-            if (!button) {
-                button = document.createElement("button");
-                button.classList.add("nav-link");
-                button.id = "familyTreeApps-tab";
-                button.setAttribute("data-bs-toggle", "tab");
-                button.setAttribute("data-bs-toggle", "tab");
-                button.setAttribute("data-bs-target", "#familyTreeApps");
-                button.setAttribute("role", "tab");
-                button.setAttribute("aria-controls", "familyTreeApps");
-                button.appendChild(document.createTextNode("Tree Apps"));
+            window.View = class View {
+                constructor() {
+                    this.id = null;
+                    Object.assign(this, this?.meta()); // this will spread object into object fields for easier access
+                }
+                meta() {
+                    return {
+                        title: "Template view",
+                        description: "Showcase of the views and their registration.",
+                        docs: "https://example.com",
+                    };
+                }
+                init(container_selector, person_id) {
+                    document.querySelector(container_selector).innerHTML = `Template View for person with ID: ${person_id}`;
+                }
+                close() { }
+            };
+            document.head.appendChild(link1);
+            document.head.appendChild(link2);
+            document.head.appendChild(script1);
+            script1.addEventListener("load", () => {
+                if (!button) {
+                    button = document.createElement("button");
+                    button.classList.add("nav-link");
+                    button.id = "familyTreeApps-tab";
+                    button.setAttribute("data-bs-toggle", "tab");
+                    button.setAttribute("data-bs-toggle", "tab");
+                    button.setAttribute("data-bs-target", "#familyTreeApps");
+                    button.setAttribute("role", "tab");
+                    button.setAttribute("aria-controls", "familyTreeApps");
+                    button.appendChild(document.createTextNode("Tree Apps"));
 
-                tab = document.createElement("div");
-                tab.id="familyTreeApps";
-                tab.classList.add("tab-pane");
-                tab.classList.add("fade");
-                tab.setAttribute("role", "tabpanel");
-                tab.setAttribute("tabindex", "0");
-                // Argh. Surely a "selectTab" method is required?
-                // Going to rely on CSS
-                tab.innerHTML = "<section>This idea is not fully fleshed out, but maybe integrating some tree apps here would be good?<br/><br/>Say, using an iframe taking up either the rest of the page (as shown), or with the regular biography/footer etc below?</section>"
-            }
+                    tab = document.createElement("div");
+                    tab.id="familyTreeApps";
+                    tab.classList.add("tab-pane");
+                    tab.classList.add("fade");
+                    tab.setAttribute("role", "tabpanel");
+                    tab.setAttribute("tabindex", "0");
+                    // Argh. Surely a "selectTab" method is required?
+                    // Going to rely on CSS
+                    button.slippyTree = new SlippyTree();
+                    let properties = {};
+                    button.addEventListener("show.bs.tab", function (event) {
+                        button.slippyTree.init(tab, document.querySelector("button[data-bs-title=\"Copy ID\"]").getAttribute("data-copy-text"), properties);
+                    })
+                    button.addEventListener("hide.bs.tab", function (event) {
+                        button.slippyTree.close();
+                    })
+                }
 
-            new bootstrap.Tab(tab);
-            let e = document.querySelector("#familyDescendants");
-            e.parentNode.insertBefore(tab, e.nextSiblingElement);
-            e = document.querySelector("#familyDescendants-tab");
-            e.parentNode.insertBefore(button, e.nextSiblingElement);
+                new bootstrap.Tab(tab);
+                let e = document.querySelector("#familyDescendants");
+                e.parentNode.insertBefore(tab, e.nextSiblingElement);
+                e = document.querySelector("#familyDescendants-tab");
+                e.parentNode.insertBefore(button, e.nextSiblingElement);
+            });
             return true;
+
         },
         deactivate: () => {
-            document.head.removeChild(link);
+            delete window.View;
+            document.head.removeChild(link1);
+            document.head.removeChild(link2);
+            document.head.removeChild(script1);
             button.parentNode.removeChild(button);
             tab.parentNode.removeChild(tab);
         },
